c58195cb48353f992faa309e4c5bff52c32213fd
souravkumar3374-dot <souravkumar3374@gmail.com>
2026-07-07 04:38:09 +0530

    Add hover effect to portfolio cards

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-console.log('Portfolio Part 1 loaded');
+document.querySelectorAll('.card').forEach(c=>{c.onmouseenter=()=>c.style.boxShadow='0 10px 30px rgba(59,130,246,.35)';c.onmouseleave=()=>c.style.boxShadow='none';});
