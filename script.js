2d6e2521717601fa142015d8871e7e05d01c9661
souravkumar3374-dot <souravkumar3374@gmail.com>
2026-07-07 23:08:33 +0530

    Add console log on script load

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-document.querySelectorAll('.grid div').forEach(x=>{x.onmouseenter=()=>x.style.transform='translateY(-6px)';x.onmouseleave=()=>x.style.transform='translateY(0)';});
+console.log('Loaded');
